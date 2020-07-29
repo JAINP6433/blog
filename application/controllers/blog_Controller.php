9b3e603dd1323bebf3c9df94d8692441632bc61f
komal jain <jaink6433@gmail.com>
2020-07-29 07:25:56 -0700

    modified files

--- application/controllers/blog_Controller.php
+++ application/controllers/blog_Controller.php
@@ -5,16 +5,64 @@ class blog_Controller extends CI_Controller {
 
 	public function __construct(){
 									parent::__construct();
+									$this->load->helper('url');
+									$this->load->model('log_Model');
+	
+
+	
 									}
    public function index()
 	{
 		die('string');
 	}
 	public function Users()
-	{
-		$this->load->helper('url');
- $this->load->view('blog/header');
+	{   
+		if($this->input->post('submit'))
+          {
+			$output = $this->log_Model->get();
+		        if($output)
+				{
+					$data['error']="<h3 style='color:red'>This user already exists</h3>";
+				}
+				else
+				{
+				    $data['error']="<h3 style='color:blue'>Your account created successfully</h3>";
+				  
+					
+				}
+		  }
+				
+	   if($this->input->post('login'))
+          {
+			$output = $this->log_Model->getlog();
+		           if($output)
+				{  
+			    	 $this->load->library('session');
+					 $this->session->set_userdata('id');
+					return redirect('blog_Controller/displaydata');
+			    }
+			else
+				{
+				  $data['error']="<h3 style='color:red'>Invalid login details</h3>";
+										           		 			    					
+			    }
+	   
+         }
+  
+		
+		 $this->load->view('blog/header');
+		 $this->load->view('blog/login');
+		  $this->load->view('blog/footer');
+
+		 }
+		 public function displaydata(){
+		 	$this->load->view('blog/header');
+		    $this->load->view('blog/dashboard');
+		    $this->load->view('blog/footer');
+
+		 				
+		 }
+ 
     }
+    ?>
     
-    
-}
---
+++ application/views/blog/dashboard.php
@@ -0,0 +1,128 @@
+<section class="content-page">
+  <div class="title_box wow fadeInUp">
+    <div class="container">
+      <div class="breadcrumbs"> <span><a class="home" href="index.html">Home</a></span>/<span>My Account</span> </div>
+      <div class="title_wr">
+        <h1>Dashboard</h1>
+      </div>
+    </div>
+  </div>
+  <section class="container">
+    <section class="white-main-box">
+      <div class="inner-content-box clearfix">
+        <div class="my-account-part">
+          <div class="left-my-account-menu-m">
+            <div class="triangleBottomRight firstItem"></div>
+            <div class="left-my-account-menu">
+              <ul>
+                <li><a href="user-dashboard.html" class="active"><i class="fa fa-home"></i> Dashboard</a></li>
+                <li><a href="my-booked-services.html"><i class="fa fa-bookmark"></i> My Booked Services</a></li>
+                <li><a href="my-favorite-services.html"><i class="fa fa-heart"></i> My Favorite Services</a></li>
+                <li><a href="my-favorite-providers.html"><i class="fa fa-list-alt"></i> My Favorite Providers</a></li>
+                <li><a href="my-reviews.html"><i class="fa fa-comment"></i> My Reviews</a></li>
+                <li><a href="user-account-settings.html"><i class="fa fa-cogs"></i> Account Settings</a></li>
+                <li><a href="index.html"><i class="fa fa-power-off"></i> Logout</a></li>
+              </ul>
+            </div>
+            <div class="triangleBottomleft firstItem"></div>
+          </div>
+          <div class="right-my-account">
+            <div class="right-my-account-blocks">
+              <div class="detail-heading">
+                <section class="title-left">
+                  <h1 style="display:inline-block;">Sophia Lopez</h1>
+                </section>
+                <div class="clearfix"></div>
+              </div>
+              <div class="right-my-account-blocks-inner">
+                <div id="profilePic">
+                  <div class="pro-pic"><img class="propic_img" src="images/profile-pic.jpg" alt="">
+                    <div class="chang-but btn-success"><a href="#">Change Photo</a>
+                      <input type="file" style="cursor:pointer;" title="" id="uploadedfile" name="profile_image">
+                    </div>
+                    <div class="wait-photo" style="display: none;" id="waiting-div"> <span>Please wait<br>
+                      <img alt="Loader" title="Loader" width="64" height="64" src="images/loading-bubbles.svg"> </span> </div>
+                  </div>
+                </div>
+                <section class="profile-info">
+                  <ul>
+                    <li><span class="s-left1">Name :</span><span class="s-right"> Sophia Lopez</span>
+                      <section class="clearfix"></section>
+                    </li>
+                    <li><span class="s-left1"> Email Address :</span><span class="s-right"> sophialopez@unv7.com</span>
+                      <section class="clearfix"></section>
+                    </li>
+                    <li><span class="s-left1">Date of Birth :</span><span class="s-right"> 22-Nov-1990</span>
+                      <section class="clearfix"></section>
+                    </li>
+                    <li><span class="s-left1"> Contact No. :</span><span class="s-right"> +44-1234546789</span>
+                      <section class="clearfix"></section>
+                    </li>
+                    <li><span class="s-left1"> Address :</span><span class="s-right"> 69 a Upper Street, San Francisco, USA </span>
+                      <section class="clearfix"></section>
+                    </li>
+                    <li><span class="s-left1">Account Status :</span><span class="s-right"> Active</span>
+                      <section class="clearfix"></section>
+                    </li>
+                    <li><span class="s-left1">Last Logged in :</span><span class="s-right"> 1 Days ago </span>
+                      <section class="clearfix"></section>
+                    </li>
+                  </ul>
+                </section>
+                <div class="clearfix"></div>
+                <div class="heading-section clearfix">
+                  <h2>Recent <span>Booking History</span></h2>
+                </div>
+                <div class="table-responsive for-service">
+                  <div class="table-responsive">
+                    <table class="table table-vcenter table-striped">
+                      <thead>
+                        <tr>
+                          <th>Service Name</th>
+                          <th>Provider</th>
+                          <th>Booking Date</th>
+                          <th>Service Status</th>
+                          <th>Review Status</th>
+                          <th>Option</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Lorem ipsum dolor sit amet</td>
+                          <td>Rob Roy Salon</td>
+                          <td>19-Aug-2015 10:00</td>
+                          <td><label class="label-danger">Pending</label></td>
+                          <td><label class="label-default">NA</label></td>
+                          <td><a href="#" class="btn btn-info"><i class="fa fa-eye"></i> View</a></td>
+                        </tr>
+                        <tr>
+                          <td>Lorem ipsum dolor sit amet</td>
+                          <td>Waterlilly Spa Center</td>
+                          <td>06-Jul-2015 11:00</td>
+                          <td><label class="label-danger">Pending</label></td>
+                          <td><label class="label-default">NA</label></td>
+                          <td><a href="#" class="btn btn-info"><i class="fa fa-eye"></i> View</a></td>
+                        </tr>
+                        <tr>
+                          <td>Lorem ipsum dolor sit amet</td>
+                          <td>Grace's Auto Mobile</td>
+                          <td>10-May-2015 10:00</td>
+                          <td><label class="label-danger">Pending</label></td>
+                          <td><label class="label-default">NA</label></td>
+                          <td><a href="#" class="btn btn-info"><i class="fa fa-eye"></i> View</a></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div class="clearfix"></div>
+              </div>
+            </div>
+            <div class="clearfix"></div>
+          </div>
+          <div class="clearfix"></div>
+        </div>
+      </div>
+    </section>
+  </section>
+</section>
