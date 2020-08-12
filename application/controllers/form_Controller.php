090f23fd86e143b93e6374eadd67de6f05095167
komal jain <jaink6433@gmail.com>
2020-08-12 06:17:14 -0700

    8 modification

--- application/controllers/form_Controller.php
+++ application/controllers/form_Controller.php
@@ -1,13 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 
-class Whatsapp extends CI_Controller {
 
-	function __construct()
-	{ 
-		
-		parent::__construct();
-		}
 
 class form_Controller extends CI_Controller {
 
